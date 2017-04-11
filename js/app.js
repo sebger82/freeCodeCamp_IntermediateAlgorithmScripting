3e43813fbdfa8b06c22f320274478ea58f4f9b2a
Sebastian Urbanski <sebger82@gmail.com>
2017-04-11 22:11:33 +0200

    tablica z roznych elementow dla dwoch tablic

--- js/app.js
+++ js/app.js
@@ -11,3 +11,24 @@ return(sum);
 }
 
 sumAll([1, 4]);
+
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  
+  for (var i=0; i < arr2.length; i++){
+    if (arr1.indexOf(arr2[i]) < 0) {
+      newArr.push(arr2[i]);
+    }
+  }
+  for (var j=0; j< arr1.length; j++) {
+    if (arr2.indexOf(arr1[j]) < 0) {
+      newArr.push(arr1[j]);
+    }
+  }
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
