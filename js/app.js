e3f60112e7c8f95ce9505b4cf23af7729df1c48f
Sebastian Urbanski <sebger82@gmail.com>
2017-04-19 19:53:44 +0200

    funkcja zwracajaca pierwsza napotkana wertosc w tablicy, zgodna z funkcja

--- js/app.js
+++ js/app.js
@@ -329,3 +329,11 @@ function smallestCommons(arr) {
 }
 
 smallestCommons([1,5]);
+
+// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+function findElement(arr, func) {
+  return arr.find(func);
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
