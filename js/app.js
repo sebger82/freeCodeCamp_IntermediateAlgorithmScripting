980f3812d7428331c09bc71492930f290f7d1228
Sebastian Urbanski <sebger82@gmail.com>
2017-04-21 12:22:32 +0200

    funkcja zwracajaca tablice pasujacych obiektow

--- js/app.js
+++ js/app.js
@@ -421,3 +421,17 @@ function addTogether() {
 }
 
 addTogether(2)(3);
+
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+function whatIsInAName(collection, source) {
+    var arr = [];
+    for (var key1 in source){} for (var key2 in collection){
+        if (source[key1] === collection[key2][key1]){
+            arr.push(collection[key2]);
+        }
+    }
+    return(arr);
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
